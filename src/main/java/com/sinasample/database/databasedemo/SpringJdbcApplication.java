272fe2c5cf0b7ad722dd4651c65728ccc19b62e4
sina <pooya.sina@gmail.com>
2020-07-23 22:44:59 +0430

    add Course entity and related entity manager with findby id method

--- src/main/java/com/sinasample/database/databasedemo/SpringJdbcApplication.java
+++ src/main/java/com/sinasample/database/databasedemo/SpringJdbcApplication.java
@@ -9,7 +9,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.sinasample.database.databasedemo.entity.Course;
 import com.sinasample.database.databasedemo.entity.Person;
+import com.sinasample.database.databasedemo.jpa.CourseRepository;
 import com.sinasample.database.databasedemo.jpa.PersonJpaRepository;
 
 @SpringBootApplication
@@ -18,16 +20,20 @@ public class SpringJdbcApplication implements CommandLineRunner{
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	@Autowired
 	private PersonJpaRepository personJpaRepository;
+	@Autowired
+	private CourseRepository courseRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJdbcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		logger.info("find one user {}" , personJpaRepository.findById(10001));
-		logger.info("create  user {}" , personJpaRepository.create(new Person("ali","istanbul",new Date())));
-		logger.info("update one user {}" , personJpaRepository.update(new Person(10001,"sina","tehran edited",new Date())));
-		logger.info("find All users {}" , personJpaRepository.findAll());
+//		logger.info("find one user {}" , personJpaRepository.findById(10001));
+//		logger.info("create  user {}" , personJpaRepository.create(new Person("ali","istanbul",new Date())));
+//		logger.info("update one user {}" , personJpaRepository.update(new Person(10001,"sina","tehran edited",new Date())));
+//		logger.info("find All users {}" , personJpaRepository.findAll());
+		Course course = courseRepository.findById(10001l);
+		logger.info("course number 10001 is {}",course);
 	}
 	
 
